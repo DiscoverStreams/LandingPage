a42453227dc48f2cf709a937b16ec1f1e05ce02d
erfan <erfanshazid@gmail.com>
2021-01-14 19:02:17 -0500

    foundations progress

---
+++ frontend/src/components/Projects/Projects.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <ProjectBlock  />
+  </div>
+</template>
+
+<script>
+import ProjectBlock from './ProjectBlock';
+
+export default {
+  name: "Projects",
+  components: {
+    ProjectBlock
+  },
+  data: () => ({
+    cities: ['Kansas/Colarado', 'California', 'Michigan']  
+    images: ['', '', '']
+  })
+};
+</script>
+
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
