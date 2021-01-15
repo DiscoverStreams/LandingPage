c4477698f8b7c6d42b3c9c70a5bf15d64bd2897f
erfan <erfanshazid@gmail.com>
2021-01-15 16:26:48 -0500

    affiliations component modifications

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -8,13 +8,13 @@
 <script>
 // @ is an alias to /src
 import Description from "@/components/Description";
-import Affiliations from "@/components/Home/Affiliations";
+import Affiliations from "@/components/Affiliations";
 
 export default {
   name: "Home",
   components: {
     Description,
-     Affiliations
+    Affiliations
   },
   data: () => ({
     description: "Groundwater pumping can lead to reductions in streamflow (‘streamflow depletion’). Streamflow depletion must be accurately quantified for effective conjunctive management of groundwater and surface water resources, and is relevant for water management questions related to the attribution of streamflow change, estimating impacts of groundwater pumping, and mitigation of streamflow depletion issues."
