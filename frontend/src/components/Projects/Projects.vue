2c159e5da9bfd8aa7158505f23c07c36f5ab9e73
erfan <erfanshazid@gmail.com>
2021-02-17 11:56:27 -0500

    carousel

--- frontend/src/components/Projects/Projects.vue
+++ frontend/src/components/Projects/Projects.vue
@@ -1,19 +1,40 @@
 <template>
   <div class="projects">
-    <div v-for="(project, index) in projects" :key="index">
+    <!-- <div v-for="(project, index) in projects" :key="index">
       <ProjectBlock :city='project.city' :image='project.image' />
+    </div> -->
+    <div class="container">
+      <carousel :settings="settings" :breakpoints="breakpoints" :wrap-around="true">
+        <slide v-for="(project, i) in projects" :key="i">
+          <ProjectBlock :city='project.city' :image='project.image' />  
+          
+        </slide>
+
+        <template #addons>
+          <navigation />
+          <pagination />
+        </template>
+      </carousel>
     </div>
+    
   </div>
-  
 </template>
 
 <script>
+import 'vue3-carousel/dist/carousel.css';
+import { Carousel, Navigation, Pagination, Slide } from 'vue3-carousel';
+
 import ProjectBlock from './ProjectBlock';
+
 
 export default {
   name: "Projects",
   components: {
-    ProjectBlock
+    ProjectBlock,
+    Carousel,
+    Slide,
+    Navigation,
+    Pagination
   },
   data: () => ({
     projects: [
@@ -30,18 +51,64 @@ export default {
         image: 'Ausable-River-Michigan.jpeg'
       },
       
-    ]
+    ],
+    // carousel settings
+    settings: {
+      itemsToShow: 1,
+      snapAlign: "center"
+    },
+    // breakpoints are mobile first
+    // any settings not specified will fallback to the carousel settings
+    breakpoints: {
+      // 700px and up
+      1200: {
+        itemsToShow: 2,
+        snapAlign: "center"
+      },
+      // 1024 and up
+      1700: {
+        itemsToShow: 3,
+        snapAlign: "center"
+      }
+    }
   })
 };
 </script>
 
 <style scoped>
 .projects{
-  display: flex;
-  flex-flow: row wrap;
+  display: unset;
+  /* flex-flow: row wrap; */
   width: 100%;
-  justify-content: space-around;
+  /* justify-content: space-around; */
   margin: 0 auto 1rem auto;
+}
+
+.container {
+  width: 80%;
+  margin: 0 auto 1rem auto;
+}
+
+.carousel__item {
+  min-height: 200px;
+  width: 100%;
+  background-color: #642afb;
+  color: white;
+  font-size: 20px;
+  border-radius: 8px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.carousel__slide {
+  /* padding: 10px; */
+}
+
+.carousel__prev,
+.carousel__next {
+  /* box-sizing: content-box;
+  border: 5px solid white; */
 }
 
 </style>
