c6c4b073e3ec40d179d322de358511b79339112d
erfan <erfanshazid@gmail.com>
2021-02-22 11:33:33 -0500

    footer + mailing list frontend

--- frontend/src/components/Footer.vue
+++ frontend/src/components/Footer.vue
@@ -24,6 +24,13 @@
         <a href="https://interactiveviz.ku.edu/DiscoverWater/" target="_blank">License Possibly</a>
       </div>
     </div>
+    <div class="mailing-list-container">
+      <div class="mailing-list">
+        <input placeholder="Email" type="email">
+        <button class="mailButton"  v-on:click="mailSubmit">Join!</button>
+    
+      </div>
+    </div>
     <div>
       &copy; {{year}} DiscoverStream
     </div>
@@ -35,12 +42,71 @@ export default {
   name: "Footer",
   data: () => ({
     year : new Date().getFullYear(),
-  })
+  }),
+  methods: {
+    mailSubmit: function (event) {
+      alert(event.target.value)
+    }
+  }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.mailing-list-container{
+  display: flex;
+  margin: 0.5rem 0;
+  justify-content: center;
+  
+  
+}
+
+.mailing-list{
+  display: flex;
+  width: 50%;
+  max-width: 50rem;
+  -webkit-transition: all 0.2s ease-in-out;
+  transition: all 0.2s ease-in-out;
+}
+
+.mailButton{
+  width: 10%;
+  min-width: 6rem;
+  background: #d3ffeb;
+  border-color: black;
+  color: black;
+  border: dotted 0.5px;
+  font-weight: bold;
+  outline: none;
+  text-transform: uppercase;
+  -webkit-transition: all 0.1s ease-in-out;
+  transition: all 0.1s ease-in-out;
+  border-top-right-radius: 10%;
+  border-bottom-right-radius: 10%;
+}
+.mailButton:hover{
+  color: white;
+  border: dashed 0.75px;
+  font-style: oblique;
+  background-color: transparent;
+  border-radius: 0%;
+}
+
+input{
+  width: 90%;
+  padding: 0.5rem 0.5rem;
+  color: white;
+  outline: none;
+  border: 2px dotted rgba(128, 128, 128, 0.322);
+  background: transparent;
+  -webkit-transition: all 0.1s ease-in-out;
+  transition: all 0.1s ease-in-out;
+}
+input:focus{
+  border: dashed 2px #d3ffeb;
+  background: rgba(112, 112, 112, 0.342);
+}
+
 .header{
   font-size: 2.5rem;
   margin: auto 0 auto 0;
@@ -127,6 +193,17 @@ a:hover{
   
 }
 @media only screen and (max-width: 500px) {
+  .mailing-list{
+    width: 100%;
+    padding: 0 2rem;
+    margin-bottom: 0.5rem;
+    min-width: 12.25rem;
+  }
+
+  .mailButton{
+    min-width: 5rem;
+  }
+
   .logo{
     display: none;
   }
