5534e76092b289f2db2984be9d6102dcc5005e29
erfan <erfanshazid@gmail.com>
2021-04-27 11:11:12 -0400

    font size upped in description

--- frontend/src/components/Description.vue
+++ frontend/src/components/Description.vue
@@ -28,6 +28,7 @@ export default {
 
 h4 {
   font-weight: 600;
+  font-size: 1.1rem;
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
 }
 
